db37545a81834ce9aa6026d71740a231351f6a16
lgweida Gu <ligu2006@gmail.com>
2023-06-29 23:16:30 +0000

    add file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        main section
+        <p>main section</p>
+        <p className="text-2xl">Large Text</p>
       </div>
 
     </main>
